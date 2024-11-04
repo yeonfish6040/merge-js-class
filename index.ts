dc77cccc28c2a4c9c06fe9889b03bb8e8842910f
Yeonjun Lee <yeonfish6040@gmail.com>
2024-11-04 22:41:03 +0900

    generic

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@
  * @param { boolean } override - will override target object? default is true
  * @Return { string } - merged object
  */
-export async function merge(to: any, from: any, override: boolean = true) {
+export async function merge<T>(to: any, from: any, override: boolean = true): Promise<T> {
     return new Promise((resolve, reject) => {
         const keys = Object.getOwnPropertyNames(from);
         keys.forEach((key, i) => {
